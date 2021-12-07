6372431ec2c02bd00ce817d6aa75c644e548643d
Tyler Lynch <tylerlynch78551@gmail.com>
2021-12-07 15:43:40 -0500

    Added confidence tests and finished models.

--- Scripts/Analysis.R
+++ Scripts/Analysis.R
@@ -29,6 +29,20 @@ data_cleaned <- data%>%rename(long=longitude, lat=latitude, date=Date.Sighted,
 
 summary(data_cleaned)
 
+
+
+### Preliminary Analysis ###
+
+# Frequency Table
+table(data_cleaned$species)
+
+# Two way contigency tables
+table(Species=data_cleaned$species, Threat=data_cleaned$threat)
+table(Species=data_cleaned$species, Exposed=data_cleaned$exposed)
+table(Exposed=data_cleaned$exposed, Threat=data_cleaned$threat)
+
+
+
 ### GRAPHS ###
 
 # Stacked bar graph of threatening sightings based on crocodile species
@@ -54,6 +68,7 @@ ggsave(filename="./Graphs/Percentage of Threatening Encounters by Exposure.png")
 time_data <- data_cleaned%>%select(date, threat)%>%filter(threat==T)%>%
   mutate(month=format(date, "%m"), year=format(date, "%Y"))%>%group_by(month, year)%>%
   summarize(threats=n())%>%mutate(month=as.numeric(month), month=factor(month.abb[month],levels=month.abb))
+
 ggplot(data=time_data, aes(x=month, y=threats, group=1)) + geom_line(color="red") + geom_point() +
   labs(title="2020 Threats per Month", x="Month", y="Total Threats") +
   theme(plot.title = element_text(hjust = 0.5))
@@ -61,12 +76,12 @@ ggplot(data=time_data, aes(x=month, y=threats, group=1)) + geom_line(color="red"
 ggsave(filename="./Graphs/2020 Threats per Month.png")
 
 # Box plot of species and their length in meters.
-ggplot(data=data_cleaned, aes(x=species,y=meters, fill="dark green")) + geom_boxplot(show.legend=F) +
-  labs(title="Distribution of Length by Species", x="Species", y="Length (meters)") +
+ggplot(data=data_cleaned, aes(y=meters, fill="dark green")) + geom_boxplot(show.legend=F) +
+  labs(title="Distribution of Largest Spotted Crocodile Length", y="Meters") +
   scale_fill_brewer(palette="Set2") +
   theme(plot.title = element_text(hjust = 0.5))
 
-ggsave(filename="./Graphs/Distribution of Length by Species.png")
+ggsave(filename="./Graphs/Distribution of Largest Spotted Crocodile Length.png")
 
 # Box plot of species and their length in meters, while also taking into account whether that species was a threat
 ggplot(data=data_cleaned, aes(x=species,y=meters, fill=threat)) + geom_boxplot() +
@@ -76,6 +91,13 @@ ggplot(data=data_cleaned, aes(x=species,y=meters, fill=threat)) + geom_boxplot()
 
 ggsave(filename="./Graphs/Distribution of Length by Species and Threatening Encounters.png")
 
+# Scatter plot between amount of crocodiles and longest crocodile.
+ggplot(data=data_cleaned, aes(x=meters, y=amount)) + geom_point(color="dark green") +
+  labs(title="Relation of Largest Length to Amount of Crocodiles Spotted", x="Largest Length", y="Amount of Crocodiles Spotted") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+ggsave(filename="./Graphs/Relation of Largest Length to Amount of Crocodiles Spotted.png")
+
 # Map of Australia. Reference: https://www.r-bloggers.com/2019/04/zooming-in-on-maps-with-sf-and-ggplot2/
 worldmap <- ne_countries(scale = 'medium', type = 'map_units', returnclass = 'sf')
 australia_df <- worldmap%>%filter(name == 'Australia')
@@ -83,7 +105,6 @@ australia_cropped <- st_crop(australia_df, xmin = 138, xmax = 154, ymin = -30, y
 australia_map <- ggplot() + geom_sf(data = australia_cropped) + theme_bw()
 
 # Creating Heat Map. Reference: https://axelhodler.medium.com/creating-a-heat-map-from-coordinates-using-r-780db4901075/
-# Couldn't figure out how to fix legends. Edited the photo of graph in paint to make the legends more visually appealing.
 australia_map  +
   stat_density2d(data=data_cleaned,  aes(x=long, y=lat, fill=..level.., alpha=..level..), geom="polygon", show.legend=F) +
   scale_fill_gradientn(colours=rev(brewer.pal(7, "Spectral"))) +
@@ -104,13 +125,25 @@ ggsave(filename="./Graphs/Map of Sightings and Amount of Crocodiles per Sighting
 # Color is based on whether it was threatening (red) or non-threatening(blue)
 threat <- data_cleaned%>%filter(threat == T)
 non_threat <- data_cleaned%>%filter(threat == F)
+
 australia_map +
   geom_point(data=threat,  aes(x=long, y=lat), color='red', size=threat$amount, alpha=.5) +
   geom_point(data=non_threat,  aes(x=long, y=lat), color='dark blue', size=non_threat$amount, alpha=.5) +
-  labs(title="Map of Threatening an Non-threatening Sightings", x="Longitude", y="Latitude") +
+  labs(title="Map of Threatening and Non-threatening Sightings", x="Longitude", y="Latitude") +
   theme(plot.title = element_text(hjust = 0.5))
 
-ggsave(filename="./Graphs/Map of Threatening an Non-threatening Sightings.png")
+ggsave(filename="./Graphs/Map of Threatening and Non-threatening Sightings.png")
+
+
+### INTERVAL TESTS ###
+
+# One-sample confidence interval of the length of crocodiles
+t.test(meters, conf.level=0.95, data=data_cleaned)
+
+# Two-sample confidence interval of the difference in length of crocodiles
+t.test(meters~species, conf.level=0.95, data=data_cleaned)
+
+
 
 ### REGRESSION MODELS ###
 
@@ -127,10 +160,12 @@ summary(lm_AICmodel)
 
 detach(package:MASS,unload=TRUE)
 
-# Logistic Regression to predict whether a crocodile is a threat based on coordinates, species, whether 
-# it is exposed, size, and the amount of crocodiles spotted.
+# Prediction of the length of a threatening Crocodylus porus that is a threat:
+predict(lm_AICmodel, data.frame(species="Crocodylus porsus", threat=T), interval="predict")
 
-glm_data <- data_cleaned%>%select(-date)%>%mutate(threat=ifelse(threat, 1, 0))
+# Logistic Regression to predict whether a crocodile is a threat based on coordinates, species, whether 
+# it is exposed, size, and the amount of crocodiles spotted, and month of sighting.
+glm_data <- data_cleaned%>%mutate(threat=ifelse(threat, 1, 0), month=as.numeric(format(date, "%m")))%>%select(-date)
 
 library(MASS)
 
@@ -143,3 +178,7 @@ summary(glm_AICmodel)
 exp(coef(glm_AICmodel))
 
 detach(package:MASS,unload=TRUE)
+
+# Prediction of whether a crocodile sighting of a Crocodylus porsus at longitude 145
+# that is fully exposed is a threat or not:
+predict(glm_AICmodel, data.frame(species="Crocodylus porsus", long=145, exposed=T), type="response")
